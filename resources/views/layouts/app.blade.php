adc6eaf509e4947e58f942525b0c064360f9cb55
satourikito <rikito1130@icloud.com>
2023-09-28 20:35:18 +0900

    app.blade編集

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-50">
+        <div class="">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
